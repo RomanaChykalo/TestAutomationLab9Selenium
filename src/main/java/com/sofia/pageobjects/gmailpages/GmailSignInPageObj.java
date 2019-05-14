100ff5873f1e93e43852c9b32b7374d7b6680d3c
Sofia Predko <sophiepredko@gmail.com>
2019-05-15 01:06:05 +0300

    Some code refactoring

--- src/main/java/com/sofia/pageobjects/gmailpages/GmailSignInPageObj.java
+++ src/main/java/com/sofia/pageobjects/gmailpages/GmailSignInPageObj.java
@@ -2,16 +2,10 @@ package com.sofia.pageobjects.gmailpages;
 
 import com.sofia.pageobjects.GeneralGmailPage;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
-import static com.sofia.utilmanager.property.Property.getProperty;
 
 public class GmailSignInPageObj extends GeneralGmailPage {
-    //private static final String SIGN_IN_PAGE = getProperty("login_page");
-
     @FindBy(xpath = "//input[@id='identifierId']")
     private WebElement usernameInput;
 
@@ -30,10 +24,6 @@ public class GmailSignInPageObj extends GeneralGmailPage {
         passwordInput.sendKeys(passwordInputValue);
         passwordInput.sendKeys(Keys.ENTER);
     }
-
-//    public void navigateToLoginPage(WebDriver driver) {
-//        driver.navigate().to(SIGN_IN_PAGE);
-//    }
 
     public String getActiveUsernameAttributeValue(){
         return activeUsername.getAttribute("innerHTML");
