63bd6fbc454f0ce6bef6495245551b13d53d78b4
igor <sas.ihor.new@gmail.com>
2019-05-14 23:42:48 +0300

    added logs

--- src/main/java/com/igor/constant/Constants.java
+++ src/main/java/com/igor/constant/Constants.java
@@ -1,9 +1,8 @@
 package com.igor.constant;
 
 public class Constants {
-    public static final int NUMBER_OF_USERS = 5;
     public static final int IMPLICIT_WAIT = 15;
-    public static final int EXPLICIT_WAIT = 25;
+    public static final int EXPLICIT_WAIT = 30;
 
     private Constants(){}
 }
--- src/test/java/com/igor/GmailTest.java
+++ src/test/java/com/igor/GmailTest.java
@@ -7,6 +7,8 @@ import com.igor.page.SentPage;
 
 import com.igor.property.Property;
 import com.igor.provider.DriverProvider;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
@@ -17,11 +19,11 @@ import java.util.Iterator;
 import java.util.UUID;
 import java.util.stream.Stream;
 
-import static com.igor.constant.Constants.NUMBER_OF_USERS;
 import static com.igor.parser.JsonParser.*;
 import static org.testng.Assert.*;
 
 public class GmailTest {
+    private static final Logger LOGGER = LogManager.getLogger(GmailTest.class);
     @BeforeMethod()
     public void setStartedPage(){
         DriverProvider.getDriver().get(Property.getProperty("initial_page"));
@@ -41,19 +43,29 @@ public class GmailTest {
     public void sendEmailTest(String username, String password) {
         final String MESSAGE_TITLE = UUID.randomUUID().toString();
         LogInPage logInPage = new LogInPage();
+        LOGGER.info("Logging in");
         logInPage.setUsername(username);
         MainPage mainPage = logInPage.setPassword(password);
+        LOGGER.info("Opening new message widget");
         NewMessageWidget newMessageWidget = mainPage.clickToComposeButton();
+        LOGGER.info("filling new letter");
         newMessageWidget.setReceiverField(getIncorrectReceiver());
         newMessageWidget.setTitleField(MESSAGE_TITLE);
         newMessageWidget.setMessageField(getMessage());
         newMessageWidget.clickToSendButton();
+        LOGGER.info("Opening alert dialog");
         assertTrue(newMessageWidget.alertDialogIsEnable());
+        LOGGER.info("closing alert dialog");
         newMessageWidget.clickToButtonOkInAlertDialog();
+        LOGGER.info("deleting incorrect receiver");
         newMessageWidget.clickToDeleteContact();
+        LOGGER.info("writing correct receiver");
         newMessageWidget.setReceiverField(getReceiver());
+        LOGGER.info("sending letter");
         newMessageWidget.clickToSendButton();
+        LOGGER.info("opening sent page");
         SentPage sentPage = mainPage.goToSentPage();
+        LOGGER.info("checking sent page");
         assertEquals(sentPage.getLetter(), MESSAGE_TITLE);
     }
 
