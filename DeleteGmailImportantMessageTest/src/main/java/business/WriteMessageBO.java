86107defc950b97013c96363b520ec6f1d187383
Sofia <sofdatc@gmail.com>
2019-05-21 16:47:30 +0300

    mark and delete important message, fix login autorization

--- DeleteGmailImportantMessageTest/src/main/java/business/WriteMessageBO.java
+++ DeleteGmailImportantMessageTest/src/main/java/business/WriteMessageBO.java
@@ -2,17 +2,15 @@ package business;
 
 import org.apache.log4j.Logger;
 import pom.GmailHomePage;
-import util.readers.Message;
-import util.readers.XMLParser;
 
 public class WriteMessageBO {
     private GmailHomePage gmailHomePage = new GmailHomePage();
     private static Logger LOG = Logger.getLogger(AutorizationBO.class.getName());
 
-    public void writeGmailMessage(Message message) throws InterruptedException {
+    public void writeGmailMessage(String res , String to, String mes) throws InterruptedException {
         LOG.info("User click compose button");
         gmailHomePage.clickComposeButton();
         LOG.info("User create new messages");
-        gmailHomePage.createMessage(message.getRecipient() , message.getSubject(), message.getMessage_text());
+        gmailHomePage.createMessage(res, mes, to);
     }
 }
